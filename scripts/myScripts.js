79b9ccd73955984093cd8a0cd6eecb2fc9e740d5
Stephanie Torok <Steph@Tibors-iMac.local>
2018-05-20 08:28:09 -0700

    rotating slider final, slider cd cover folger, colored banners for individual pages

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -12,5 +12,30 @@ $(document).ready(function(){
         if (slideIndex > slides.length) {slideIndex = 1}
         slides[slideIndex-1].style.display = "block";
         setTimeout(showSlides, 5000); // Change image every 2 seconds
-    }
+    };
+
+
+    $("#flip").click(function(){
+        $("#panel").fadeToggle(1000);
+    });
+
+    $(function(){
+	  $('.rotating-slider').rotatingSlider({
+//	    // auto play
+//	    autoRotate: true,
+//	    // auto play interval
+//	    autoRotateInterval: 6000,
+//        // is draggable?
+//	    draggable: true,
+//	    // slider controls
+//	    directionControls: true,
+//	    directionLeftText: '&lsaquo;',
+//	    directionRightText: '&rsaquo;',
+//	    // animation speed
+//	    rotationSpeed: 750,
+//	    // size of slider
+//	    slideHeight : 360,
+//	    slideWidth : 480,
+	});
+
  });
