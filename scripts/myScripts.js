c26ddbd4462bfc2a17ea0039406bd5aca32c20e3
Stephanie Torok <Steph@Tibors-iMac.local>
2018-05-20 17:27:07 -0700

    small changes, sass organised

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -11,31 +11,12 @@ $(document).ready(function(){
         slideIndex++;
         if (slideIndex > slides.length) {slideIndex = 1}
         slides[slideIndex-1].style.display = "block";
-        setTimeout(showSlides, 5000); // Change image every 2 seconds
+        setTimeout(showSlides, 4000); // Change image every 2 seconds
     };
 
 
     $("#flip").click(function(){
         $("#panel").fadeToggle(1000);
     });
-
-    $(function(){
-	  $('.rotating-slider').rotatingSlider({
-//	    // auto play
-//	    autoRotate: true,
-//	    // auto play interval
-//	    autoRotateInterval: 6000,
-//        // is draggable?
-//	    draggable: true,
-//	    // slider controls
-//	    directionControls: true,
-//	    directionLeftText: '&lsaquo;',
-//	    directionRightText: '&rsaquo;',
-//	    // animation speed
-//	    rotationSpeed: 750,
-//	    // size of slider
-//	    slideHeight : 360,
-//	    slideWidth : 480,
-	});
 
  });
